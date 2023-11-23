38a9c1b25b27f01c2174f6bd2244ae231d98a954
mheftie296 <45861926+mheftie296@users.noreply.github.com>
2023-11-22 21:52:22 -0600

    Code Upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, send_file, request, redirect
+import os
+import sys
+
+app = Flask(__name__, static_folder='static', static_url_path='')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def notfound(e):
+    return render_template('404.html')
+
+@app.route('/wallpaper')
+def getwallpaper():
+    url = request.args.get('url')
+    if url is None:
+        url = ''
+    resize = request.args.get('resize','')
+    if resize is None:
+        resize = ''
+    color = request.args.get('color','')
+    if color is None:
+        color = ''
+    if request.args.get('update') is not None:
+        return send_file('/home/ubuntu/wallpaper/wallpaper.png')
+    if 'pad' in resize:
+        if url == "":
+            url = "nourl"
+        os.system('(cd /home/ubuntu/wallpaper && python3 update-pad.py '+url+' '+color+')')
+    else:
+        os.system('(cd /home/ubuntu/wallpaper && python3 update.py '+url+')')
+    return send_file('/home/ubuntu/wallpaper/wallpaper.png')
+
+if __name__ == "__main__":
+   app.run()
---
+++ pyapp.wsgi
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import sys
+sys.path.insert(0, '/var/www/pyapp')
+
+from app import app as application
